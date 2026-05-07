06efbf8280e057f1cb3310db24cc6b2390034b34
bibonix <bibonix@users.noreply.github.com>
2026-05-07 06:30:31 +0000

    Add unit test for Output to satisfy surefire's failIfNoTests
    Without at least one test class the surefire run fails after the
    parent POM upgrade. Also adds the required package-info.java for
    the test package to keep qulice's JavadocPackageCheck happy.

---
+++ src/test/java/com/jcabi/latex/maven/plugin/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2012-2026 jcabi.com
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/**
+ * Tests for jcabi-latex-maven-plugin.
+ *
+ * @since 1.0
+ */
+package com.jcabi.latex.maven.plugin;
