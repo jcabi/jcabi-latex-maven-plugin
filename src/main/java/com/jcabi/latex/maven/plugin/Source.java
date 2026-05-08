a0210b3a9cbe11f5f0a053d2a6cad847fd08b19d
Yegor Bugayenko <yegor256@gmail.com>
2026-05-08 17:54:46 +0300

    more checks

--- src/main/java/com/jcabi/latex/maven/plugin/Source.java
+++ src/main/java/com/jcabi/latex/maven/plugin/Source.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2009-2017, jcabi.com
+ * Copyright (c) 2009-2026 Yegor Bugayenko
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
